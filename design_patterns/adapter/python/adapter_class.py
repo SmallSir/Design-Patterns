06c506de9bc07b7b0337f0c9b9b250ff21c8bd4d
qiuzhenhao.smallsir <qiuzhenhao.smallsir@bytedance.com>
2020-09-12 18:43:01 +0800

    享元、门面、适配器
    Change-Id: If001730b065674945f623d0d87792cc8897246ff

---
+++ design_patterns/adapter/python/adapter_class.py
@@ -0,0 +1,32 @@
+
+class Target(object):
+    def request(self):
+        return "Target: The default target's behavior."
+
+class Adaptee(object):
+    def specific_request(self):
+        return ".eetpadA eht fo roivaheb laicepS"
+
+
+class Adapter(Target, Adaptee):
+    def request(self):
+        return f"Adapter: (TRANSLATED) {self.specific_request()[::-1]}"
+
+def client_code(target):
+    print(target.request)
+
+
+if __name__ == "__main__":
+    print("Client: I can work just fine with the Target objects:")
+    target = Target()
+    client_code(target)
+    print("\n")
+
+    adaptee = Adaptee()
+    print("Client: The Adaptee class has a weird interface. "
+          "See, I don't understand it:")
+    print(f"Adaptee: {adaptee.specific_request()}", end="\n\n")
+
+    print("Client: But I can work with it via the Adapter:")
+    adapter = Adapter()
+    client_code(adapter)
